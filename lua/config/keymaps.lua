e3cac9f3853ec0aba8b3a479379dd1d167f1eeef
Manuel Feucht <feucht.manuel@gmail.com>
2026-03-12 23:03:59 +0100

    move keymaps to keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,6 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' 
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>q', '<cmd>bn | bd #<CR>', { desc = 'Aktuellen Buffer schließen' })
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = "Fehlermeldung anzeigen" })
+vim.keymap.set('n', '<leader>rr', '<cmd>OverseerRun<CR> ', { desc = "Task ausführen" })
+vim.keymap.set('n', '<leader>rt', '<cmd>OverseerToggle<cr>', { desc = "Task-Liste (unten/seitlich)" })
+  
