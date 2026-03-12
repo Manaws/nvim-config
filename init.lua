a5e026f79ba881af1736b0d7c026fa5e2441498c
Manuel Feucht <feucht.manuel@gmail.com>
2026-03-12 21:27:23 +0100

    show row numbers

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 require("config.lazy")
 require("config.keymaps")
+
+vim.opt.number = true
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,4 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>q', '<cmd>bn | bd #<CR>', { desc = 'Aktuellen Buffer schließen' })
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = "Fehlermeldung anzeigen" })
