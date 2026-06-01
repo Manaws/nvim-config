f2a0f55024c4ce68d4014d8ec40e201c31a054d2
Manuel Feucht <feucht.manuel@gmail.com>
2026-06-01 19:44:17 +0200

    add gopher

---
+++ lua/plugins/gopher.lua
@@ -0,0 +1,9 @@
+return {
+  "olexsmir/gopher.nvim",
+  ft = "go",
+  -- branch = "develop"
+  -- (optional) updates the plugin's dependencies on each update
+  ---@module "gopher"
+  ---@type gopher.Config
+  opts = {},
+} 
