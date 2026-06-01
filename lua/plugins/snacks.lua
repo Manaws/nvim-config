a44523c7cc285358222f8624aafef5a1482cb38f
Manuel Feucht <feucht.manuel@gmail.com>
2026-06-01 19:45:57 +0200

    add hiddenfiles in explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -41,7 +41,12 @@ return {
       enabled = true,
       timeout = 3000,
     },
-    picker = { enabled = true },
+    picker = {
+      enabled = true,
+      sources = {
+        explorer = { hidden = true },
+      },
+    },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
